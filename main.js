41c365414139438a999730b77d981f4803e41ada
Kiboiko <kirboy2013@gmail.com>
2025-11-06 17:05:42 +0300

    commentsPage

--- main.js
+++ main.js
@@ -36,7 +36,6 @@ class ProductManager {
 
   // Метод для создания HTML карточки
   createProductCard(product) {
-    console.log(this.getLongestDescription());
     let formatDescription =
       product.description +
       " ".repeat(this.getLongestDescription() - product.description.length);
@@ -65,9 +64,46 @@ class ProductManager {
   }
 }
 
+class CommentsManager {
+  constructor() {
+    this.comments = [];
+    this.container = document.getElementById("commentsContainer");
+  }
+
+  async loadComments() {
+    try {
+      const response = await fetch("comments.json");
+      this.comments = await response.json();
+      this.renderComments();
+      console.log(this.comments);
+    } catch (error) {
+      console.error("Ошибка загрузки комментариев:", error);
+    }
+  }
+
+  createCommentCard(comment) {
+    // Исправлено: параметр comment вместо product
+    return `
+            <div class="comment-card" data-id="${comment.id}">
+                <img src="${comment.image}" alt="${comment.name}" class="comment-image">
+                <div class="comment-description">${comment.description}</div>
+                <div class="comment-name">${comment.name}</div> <!-- Исправлено: comment.name вместо product.name -->
+            </div>
+        `;
+  }
+
+  renderComments() {
+    this.container.innerHTML = this.comments
+      .map((comment) => this.createCommentCard(comment))
+      .join("");
+  }
+}
+
 const productManager = new ProductManager();
+const commentsManager = new CommentsManager();
 
 // Загружаем товары при загрузке страницы
 document.addEventListener("DOMContentLoaded", () => {
   productManager.loadProducts();
+  commentsManager.loadComments();
 });
